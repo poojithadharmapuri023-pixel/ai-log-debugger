b56fb2e299a0f2b4b7605e0ccd9e2e6d582ff42e
poojithadharmapuri <poojithadharmapuri023@gmail.com>
2026-09-26 18:20:35 +0530

    Add ReDoc endpoint test

--- tests/test_api.py
+++ tests/test_api.py
@@ -129,3 +129,10 @@ def test_docs_endpoint():
 
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
+
+
+def test_redoc_endpoint():
+    response = client.get("/redoc")
+
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
